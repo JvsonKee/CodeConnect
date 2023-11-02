0eae2e53f47ea00e80ee44af77c7ca9e7f1c0fb6
JvsonKee <jasonk2723@gmail.com>
2023-11-02 13:13:04 -0600

    created trending topics carousel in explore page

--- client/src/components/ExplorePage.jsx
+++ client/src/components/ExplorePage.jsx
@@ -2,11 +2,14 @@ import Dropdown from "./Dropdown"
 import NavBar from "./NavBar"
 import SearchBar from "./SearchBar"
 import { Container } from "./styles/Container"
-import { CheckboxContainer, CheckboxLabel, ContentContainer, ExplorePageContainer, FireIcon, TopicsCheckbox, TopicsContainer, TopicsHeaderContainer, TopicsSection } from "./styles/ExplorePage.styled"
+import { ContentContainer, ExplorePageContainer, FireIcon, CarouselContainer, TopicsHeaderContainer, TopicsSection, CarouselImage, CarouselSlide, CarouselLabel, Carousel, CarouselNav } from "./styles/ExplorePage.styled"
 import { useState } from "react"
 import { PageHeader } from "./styles/PageHeader"
 import TopicBubble from "./TopicBubble"
 import { faFire } from "@fortawesome/free-solid-svg-icons"
+import webDevPicture from "../assets/web-dev.png"  // https://www.google.com/search?q=web+development&sca_esv=578888506&rlz=1C5CHFA_enCA1022CA1022&tbm=isch&sxsrf=AM9HkKleoNTmOFYy1L2asBqWBw79IrDHJw:1698948188574&source=lnms&sa=X&ved=2ahUKEwj19ZWa86WCAxWaIDQIHdNrDysQ_AUoAXoECAEQAw&biw=1800&bih=986&dpr=2#imgrc=O0Or2_DpELZxxM
+import gameDevPicture from "../assets/game-dev.png" // source: https://itchronicles.com/artificial-intelligence/ai-in-game-development/
+import programmingPicture from "../assets/programming.png" // source: https://www.google.com/search?q=java&tbm=isch&hl=en&chips=q:java,g_1:programming:R3yWj66rrrk%3D&rlz=1C5CHFA_enCA1022CA1022&sa=X&ved=2ahUKEwjSgKaQ8qWCAxWUFTQIHWfNDnkQ4lYoAXoECAEQMQ&biw=1800&bih=986#imgrc=3VOAR5G6kgBfrM&imgdii=c3KSenntr082qM
 
 function ExplorePage() {
     const [status, setStatus] =  useState('Recent')
@@ -24,22 +27,29 @@ function ExplorePage() {
                             <PageHeader>
                                 <div> <FireIcon icon={faFire}/> Trending</div>
                             </PageHeader>
-                            <CheckboxContainer>
-                                <CheckboxLabel>Select</CheckboxLabel>
-                                <TopicsCheckbox type="checkbox" id="topics-checkbox"/>
-                            </CheckboxContainer>
                         </TopicsHeaderContainer>
-                        <TopicsContainer>
-                            <TopicBubble topicName={"Web Development"}/>
-                            <TopicBubble topicName={"React"}/>
-                            <TopicBubble topicName={"Python"}/>
-                            <TopicBubble topicName={"Dynamic Programming"}/>
-                            <TopicBubble topicName={"JavaScript"}/>
-                            <TopicBubble topicName={"Full-Stack Development"}/>
-                            <TopicBubble topicName={"Data Structures"}/>
-                            <TopicBubble topicName={"Networks"}/>
-                            <TopicBubble topicName={"Game Development"}/>
-                        </TopicsContainer>
+                        <CarouselContainer>
+                            <Carousel>
+                                <CarouselSlide id="slide-1">
+                                    <CarouselImage src={webDevPicture} />
+                                    <CarouselLabel>Web Development</CarouselLabel>
+                                </CarouselSlide>
+                                <CarouselSlide id="slide-2">
+                                    <CarouselImage src={gameDevPicture}/>
+                                    <CarouselLabel>Game Development</CarouselLabel>
+                                </CarouselSlide>
+                                <CarouselSlide id="slide-3"> 
+                                    <CarouselImage src={programmingPicture}/>
+                                    <CarouselLabel>Java</CarouselLabel>
+                                </CarouselSlide>      
+                                <CarouselNav>
+                                    <a href="#slide-1"></a>
+                                    <a href="#slide-2"></a>
+                                    <a href="#slide-3"></a>
+                                </CarouselNav>            
+                            </Carousel>  
+
+                        </CarouselContainer>
                     </TopicsSection>
                 </ContentContainer>
             </ExplorePageContainer>
--- client/src/components/styles/ExplorePage.styled.js
+++ client/src/components/styles/ExplorePage.styled.js
@@ -4,9 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 export const ExplorePageContainer = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
     margin-left: 20vw;
     width: 80vw;
-    height: 100%;
+    height: 100vh;
     background-color: var(--black);
 `
 
@@ -16,6 +17,7 @@ export const ContentContainer = styled.div`
     gap: 100px;
     padding: 5vh 0;
     width: 80%;
+    height: 80%;
 `
 
 export const DropdownContainer = styled.div`
@@ -28,37 +30,79 @@ export const TopicsSection = styled.div`
     display: flex;
     flex-direction: column;
     gap: 30px;
+    height: 80%;
 `
 
 export const TopicsHeaderContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    width: 100%;
+    width: 90%;
+    margin: 0 auto;
 `
 
 export const FireIcon = styled(FontAwesomeIcon)`
 
 `
 
-export const CheckboxContainer = styled.div`
+export const CarouselContainer = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+`
+
+export const Carousel = styled.div`
     display: flex;
-    align-items: center;
-    gap: 10px;
+    margin: 0 auto;
+    overflow-x: auto;
+    width: 950px;
+    scroll-snap-type: x mandatory;
+    scroll-behavior: smooth;
+`
+
+export const CarouselSlide = styled.div`
+    width: 100%;
+    position: relative;
+    text-align: center;
     color: var(--white);
 `
 
-export const CheckboxLabel = styled.div`
-
+export const CarouselImage = styled.img`
+    flex: 3;
+    max-width: 950px;
+    scroll-snap-align: start;
+    filter: brightness(50%);
+    border-radius: 10px;
 `
 
-export const TopicsCheckbox = styled.input`
-
+export const CarouselLabel = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 50px;
+    font-weight: 600;
 `
 
-export const TopicsContainer = styled.div`
+export const CarouselNav = styled.div`
     display: flex;
-    flex-wrap: wrap;
-    gap: 20px 2%;
-    width: 100%;
+    gap: 10px;
+    position: absolute;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
+
+    a {
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background-color: var(--white);
+        opacity: 0.75;
+        transition: opacity ease 250ms;
+
+        &:hover {
+            opacity: 100%;
+        }
+    }
 `
