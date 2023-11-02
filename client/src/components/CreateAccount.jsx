02327ec41ce5f61091be7f8035cb365ebb46b234
jcacierto <78886859+jcacierto@users.noreply.github.com>
2023-11-01 19:09:41 -0600

    commented out import Modal
    This version works.

--- client/src/components/CreateAccount.jsx
+++ client/src/components/CreateAccount.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-
+//import { Modal } from "react-bootstrap/Modal";
 
 
 function CreateAccount() {
