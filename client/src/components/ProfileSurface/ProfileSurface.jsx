6804280f913447626c6f5d143ea65230b795ec48
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-11-03 00:38:55 -0600

    Changed up profile surface a bit

--- client/src/components/ProfileSurface/ProfileSurface.jsx
+++ client/src/components/ProfileSurface/ProfileSurface.jsx
@@ -2,13 +2,33 @@ import React from "react";
 import ProfileSurfaceStyle from "./ProfileSurfaceStyle.style";
 import CircularImage from "../CircularImage/CircularImage";
 import ProfilePic from "../../assets/profile-pic.jpg";
-import UserNameSurface from "../UserNameSurface/UsernameSurface";
+import DisplaySurfaceStyle from "../styles/DisplaySurfaceStyle.styled";
+import Headline from "../typography/Headline";
+import { TagStyle, TagText } from "../styles/TagStyle";
+import { BioStyle } from "../styles/BioStyle.styled";
+import Body from "../typography/Body";
+import { HeaderRow } from "../styles/HeaderRow";
 
 function ProfileSurface(){
     return <ProfileSurfaceStyle>
                 <CircularImage src={ProfilePic} alt="User Profile Picture"/>
-                <UserNameSurface/>
+                <DisplaySurfaceStyle>
+                    <Headline text={"PixelPioneer"}/>
+                    <Headline text={"(She/Her)"}/>
+                </DisplaySurfaceStyle>
+                <DisplaySurfaceStyle>
+                    <TagStyle>
+                        <TagText>Teacher</TagText>
+                    </TagStyle>
+                    <TagStyle>
+                        <TagText>Web Development, Full-Stack, +1 More</TagText>
+                    </TagStyle>
+                </DisplaySurfaceStyle>
+                <BioStyle>
+                <Body text={"Oh wow this is my bio!!!"}/>
+                </BioStyle>
             </ProfileSurfaceStyle>;
+            
 }
 
 export default ProfileSurface;
--- client/src/components/ProfileSurface/ProfileSurfaceStyle.style.js
+++ client/src/components/ProfileSurface/ProfileSurfaceStyle.style.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 const ProfileSurfaceStyle = styled.div`
     display: flex;
     padding: 16px;
+    max-width: 500px;
+    mi-width: 500px;
     flex-direction: column;
     align-items: center;
     background-color: #121212;
