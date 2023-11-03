61fc055812bf3e11cc4794df738878e0602d1177
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-11-02 22:42:10 -0600

    Added username surface to greater profile surface

--- client/src/components/ProfileSurface/ProfileSurface.jsx
+++ client/src/components/ProfileSurface/ProfileSurface.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ProfileSurfaceStyle from "./ProfileSurfaceStyle.style";
 import CircularImage from "../CircularImage/CircularImage";
 import ProfilePic from "../../assets/profile-pic.jpg";
+import UserNameSurface from "../UserNameSurface/UsernameSurface";
 
 function ProfileSurface(){
     return <ProfileSurfaceStyle>
                 <CircularImage src={ProfilePic} alt="User Profile Picture"/>
+                <UserNameSurface/>
             </ProfileSurfaceStyle>;
 }
 
