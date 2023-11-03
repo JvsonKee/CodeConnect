fbf77f811fce4e56a1e2765547059184d60dbfdf
JvsonKee <jasonk2723@gmail.com>
2023-11-02 23:37:13 -0600

    removed extra post time

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -14,7 +14,7 @@ const user1 = {
     userName: "JasonSucksAtCoding69",
     title: "Learning React",
     time: "19 minutes ago",
-    description: "I need to learn React for a project for one of my courses, what are the best resources to learn from?",
+    description: "I am learning React for a school project, what are the best resources to learn from?",
     topic: "React",
     likes: "43",
     comments: "59"
@@ -25,7 +25,7 @@ const user2 = {
     userName: "RealEyesRealizeRealLies",
     title: "Growtopia - My new game!",
     time: "1hr ago",
-    description: "I've been developing a farming game, Growtopia, for the last 2 years. I am excited to finally announce a public beta. I would appreciate if you guys checked it out!",
+    description: "I've been developing my farming game, Growtopia, for the last 2 years. I am excited to finally announce a public beta. I would appreciate if you guys checked it out!",
     topic: "Game Development",
     likes: "203",
     comments: "122"
