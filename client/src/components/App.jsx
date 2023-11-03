7ba4de5bbf5cd2940489bc78ae026ca6b8dea79f
jcacierto <78886859+jcacierto@users.noreply.github.com>
2023-11-02 21:06:57 -0600

    Made changes to Explore Results page, added placeholder photo, and fixed issues

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Container>
       <Normalizer />
-      <CreateAccount />
+      <ExploreResults />
     </Container>
   )
 }
--- client/src/components/ExploreResults.jsx
+++ client/src/components/ExploreResults.jsx
@@ -1,9 +1,16 @@
 import { Container } from "./styles/Container";
 import NavBar from "./NavBar";
-import { ExploreResultsContainer, CustomCard } from "./styles/ExploreResultsContainer.styled";
+import { ExploreResultsContainer, CustomCard, CustomCardBody, CustomCardText, InteractButtons } from "./styles/ExploreResultsContainer.styled";
 import { Card } from "react-bootstrap";
 import SearchBar from "./SearchBar"
+import { Image } from 'react-bootstrap';
 
+import CommentIcon from '@mui/icons-material/Comment';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+import profilePic from './images/profile-pic.jpg';
 
 
 function CreateAccount() {
@@ -13,36 +20,137 @@ function CreateAccount() {
             <ExploreResultsContainer>
 
                 <SearchBar/>
+
                 <CustomCard>
-                    <Card.Header as = "h5"> Featured </Card.Header>
-                    <Card.Body>
-                        <Card.Title> Post title </Card.Title>
-                        <Card.Text> Some sentence here blablabla. </Card.Text>
-                    </Card.Body>
+                    <CustomCardBody>
+    
+                        <span class = 'img-span'>
+                            <Image class = 'profileImg' src = {profilePic} width = '60px' height = '60px' roundedCircle />
+                        </span>
+
+                        <p class = "username">hackerman123</p>
+
+                        <span class = 'topic'>
+                            <p>Programming</p>
+                        </span>
+                                
+                        <CustomCardText> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+                            id est laborum
+                        </CustomCardText>
+
+                        <InteractButtons>
+                            <span> <ThumbUpIcon /> </span>
+                            <span> <CommentIcon/> </span>
+                            <span><EmojiEmotionsIcon /></span>
+                            <span> <MoreHorizIcon /></span>  
+                        </InteractButtons>
+                        
+
+                    </CustomCardBody>
                 </CustomCard>
 
                 <CustomCard>
-                    <Card.Header as = "h5"> Featured </Card.Header>
-                    <Card.Body>
-                        <Card.Title> Post title </Card.Title>
-                        <Card.Text> Some sentence here blablabla. </Card.Text>
-                    </Card.Body>
+                    <CustomCardBody>
+    
+                        <span class = 'img-span'>
+                            <Image class = 'profileImg' src = {profilePic} width = '60px' height = '60px' roundedCircle />
+                        </span>
+
+                        <p class = "username">hackerman123</p>
+
+                        <span class = 'topic'>
+                            <p>Programming</p>
+                        </span>
+                                
+                        <CustomCardText> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+                            id est laborum
+                        </CustomCardText>
+
+                        <InteractButtons>
+                            <span> <ThumbUpIcon /> </span>
+                            <span> <CommentIcon/> </span>
+                            <span><EmojiEmotionsIcon /></span>
+                            <span> <MoreHorizIcon /></span>  
+                        </InteractButtons>
+                        
+
+                    </CustomCardBody>
                 </CustomCard>
 
                 <CustomCard>
-                    <Card.Header as = "h5"> Featured </Card.Header>
-                    <Card.Body>
-                        <Card.Title> Post title </Card.Title>
-                        <Card.Text> Some sentence here blablabla. </Card.Text>
-                    </Card.Body>
+                    <CustomCardBody>
+    
+                        <span class = 'img-span'>
+                            <Image class = 'profileImg' src = {profilePic} width = '60px' height = '60px' roundedCircle />
+                        </span>
+
+                        <p class = "username">hackerman123</p>
+
+                        <span class = 'topic'>
+                            <p>Programming</p>
+                        </span>
+                                
+                        <CustomCardText> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+                            id est laborum
+                        </CustomCardText>
+
+                        <InteractButtons>
+                            <span> <ThumbUpIcon /> </span>
+                            <span> <CommentIcon/> </span>
+                            <span><EmojiEmotionsIcon /></span>
+                            <span> <MoreHorizIcon /></span>  
+                        </InteractButtons>
+                        
+
+                    </CustomCardBody>
                 </CustomCard>
 
                 <CustomCard>
-                    <Card.Header as = "h5"> Featured </Card.Header>
-                    <Card.Body>
-                        <Card.Title> Post title </Card.Title>
-                        <Card.Text> Some sentence here blablabla. </Card.Text>
-                    </Card.Body>
+                    <CustomCardBody>
+    
+                        <span class = 'img-span'>
+                            <Image class = 'profileImg' src = {profilePic} width = '60px' height = '60px' roundedCircle />
+                        </span>
+
+                        <p class = "username">hackerman123</p>
+
+                        <span class = 'topic'>
+                            <p>Programming</p>
+                        </span>
+                                
+                        <CustomCardText> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+                            id est laborum
+                        </CustomCardText>
+
+                        <InteractButtons>
+                            <span> <ThumbUpIcon /> </span>
+                            <span> <CommentIcon/> </span>
+                            <span><EmojiEmotionsIcon /></span>
+                            <span> <MoreHorizIcon /></span>  
+                        </InteractButtons>
+                        
+
+                    </CustomCardBody>
                 </CustomCard>
 
 
