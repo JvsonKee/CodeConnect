366a4ab83c29ab53ea2d1eae5d5784d1acc280d2
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-11-02 21:03:31 -0600

    Added to the UserPage Container for flexibility

--- client/src/components/styles/UserPage.styled.js
+++ client/src/components/styles/UserPage.styled.js
@@ -4,4 +4,7 @@ export const UserPageContainer = styled.div`
     width: 80vw;
     height: 100vh;  
     background-color: #242424;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
 `
