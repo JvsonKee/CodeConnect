d0497cc66a657d25378f10f03e5d3fb4d7ef9ce1
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-11-02 21:03:54 -0600

    Changed the UserPage for UI change

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -2,13 +2,17 @@ import { Container } from "../components/styles/Container";
 import { UserPageContainer } from "../components/styles/UserPage.styled";
 import NavBar from "../components/NavBar";
 import React from "react";
+import { EditProfileContainer } from "../components/styles/EditProfileContainer.styled";
+import ProfileSurface from "../components/ProfileSurface/ProfileSurface";
 
 function UserPage(){
     return (  
         <Container>
             <NavBar />  
             <UserPageContainer>
-                
+                <EditProfileContainer/>
+                <ProfileSurface/>
+                <EditProfileContainer/>
             </UserPageContainer>
         </Container>
     )
