e568786bc39a4e6f634e034d7fb3ce3264a09958
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-11-03 00:40:42 -0600

    Setup styles for headers and the central column

---
+++ client/src/components/styles/HeaderRow.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const HeaderRow = styled.div`
+    display: flex;
+    padding: 14px 0px 14px 16px;
+    align-items: center;
+    gap: 8px;
+    align-self: stretch;
+    border-bottom: 0.25px solid #FFF;
+    background: #121212;
+`
