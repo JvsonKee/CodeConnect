d1a2d51657fb8d534c7bc770673bb49fc7c95019
JvsonKee <jasonk2723@gmail.com>
2023-11-25 21:21:14 -0700

    linked pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,9 @@
-import UserPage from "./pages/Account/UserPage"
-import ExplorePage from "./pages/Explore/ExplorePage"
 import HomePage from "./pages/Home/Homepage"
 import { Container } from "./styles/Container"
-import GlobalStyles from "./styles/Global"
 function App() {
 	return (
 		<Container>
-			<GlobalStyles />
-			{/* <HomePage /> */}
-      {/* <UserPage/> */}
-			<ExplorePage />
+			<HomePage />
 		</Container>
 	)
 }
