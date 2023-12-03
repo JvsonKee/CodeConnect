6b97cdcea184883355e4cf032383dc5e6667fd91
JvsonKee <jasonk2723@gmail.com>
2023-12-02 19:14:59 -0700

    created user and post classes

---
+++ src/database/Post.js
@@ -0,0 +1,35 @@
+class Post {
+    constructor(author, title, content, topic,  timeStamp, id) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.topic = topic;
+        this.timeStamp = timeStamp;
+        this.id = id;
+        this.likes = 0;
+        this.comments = [];
+    }
+
+    like() {
+        this.likes++;
+    }
+
+    addComment(comment) {
+        this.comments.push(comment);
+    }
+
+    getPostInformation() {
+        return {
+            author: this.author,
+            title: this.title,
+            content: this.content,
+            topic: this.topic,
+            timeStamp: this.timeStamp,
+            id: this.id,
+            likes: this.likes,
+            comments: this.comments
+        }
+    }
+}
+
+export default Post
