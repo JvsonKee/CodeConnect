83a43d29851ae1ef14cdb5d308803b240867ae7a
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-12-03 18:21:28 -0700

    Added new surfaces for the user page

---
+++ src/pages/Account/UserBanner/UserBanner.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components"
+
+const UserBannerContainer = styled.div`
+    display: flex;
+    padding: 36px 80px;
+    justify-content: space-between;
+    align-items: center;
+    align-self: stretch;
+    background-color: #353845;
+`
+
+function UserBanner({user}){
+    return (  
+        <UserBannerContainer>
+
+        </UserBannerContainer>
+    )
+}
+
+export default UserBanner;
