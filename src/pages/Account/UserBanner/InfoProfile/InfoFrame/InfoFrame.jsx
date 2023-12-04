018db286cef7d56215386ae9191758da5cd5ac8a
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-12-04 10:52:15 -0700

    InfoFrame uses user social media

--- src/pages/Account/UserBanner/InfoProfile/InfoFrame/InfoFrame.jsx
+++ src/pages/Account/UserBanner/InfoProfile/InfoFrame/InfoFrame.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components"
 import H1 from "../../../typography/h1";
 import H6 from "../../../typography/h6";
+import SocialLink from "./SocialLink";
+
 
 const InfoFrameContainer = styled.div`
     display: flex;
@@ -16,7 +18,11 @@ function InfoFrame({username, biography, socialLinks}){
         <InfoFrameContainer>
             <H1>{username}</H1>
             <H6>{biography}</H6>
-
+            {
+                socialLinks.map((it, i) => (
+                    <SocialLink key={i} network={it.getSocialMedia()} username={it.getUsername()}/>
+                ))
+            }
         </InfoFrameContainer>
     )
 }
